df8438166151cd1961ac53bf08dea76b6c397bc1
tarant1no322 <cyberkivin322svet@gmail.com>
2022-11-24 02:37:30 +0300

    stsrt work with streams

--- StudyApps/24.11/Program.cs
+++ StudyApps/24.11/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+FileStream file;
+string str, str_in, pash = @"C:\Users\UserOne\Desktop\123.txt";
+
+try
+{
+    file = new FileStream(pash, FileMode.OpenOrCreate);
+}
+catch(IOException ex)
+{
+    Console.WriteLine("Возникла ошибка:" + ex.Message);
+    return;
+}
+StreamWriter out_file = new StreamWriter(file);
+try
+{
+    Console.WriteLine("Введи текст, а по завершению -- стоп(stop)");
+    do
+    {
+        Console.Write(": ");
+        str = Console.ReadLine();
+        if ((str != "стоп") & (str != "stop"))
+        {
+            str = str + "\r\n"; 
+            out_file.Write(str);
+        }
+    }
+    while ((str != "стоп") & (str != "stop"));
+}
+catch(IOException ex)
+{
+    Console.WriteLine("Возникла ошибка:" + ex.Message);
+}
+finally
+{
+    out_file.Close();
+}
+
+//StreamReader fstr_in = new StreamReader(file);
+//try
+//{
+//    while ((str_in = fstr_in.ReadLine()) != null)
+//    {
+//        Console.WriteLine(str_in);
+//    }
+//}
+//catch(IOException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//finally
+//{
+//    fstr_in.Close(); 
+//}    
+    
