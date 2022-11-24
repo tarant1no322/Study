5f4854efe4c9cd4c1173d02310fe544c40669022
tarant1no322 <cyberkivin322svet@gmail.com>
2022-11-25 00:09:43 +0300

    add project with parse double

---
+++ StudyApps/24.11(1)/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+int n = 0;
+//string s;
+double sum = 0.0, t;
+
+Console.WriteLine("Укажите желаемое количество чисел для ввода:");
+try
+{
+    n = Int32.Parse(Console.ReadLine());
+}
+catch(FormatException ex)
+{
+    Console.WriteLine("Возникла ошибка " + ex.Message);
+    return;
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Возникла ошибка " + ex.Message);
+    return;
+}
+
+Console.WriteLine("Введите по очереди " + n + "чисел:");
+for(int i = 0; i < n; i++)
+{
+    try
+    {
+        t = Double.Parse(Console.ReadLine());
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Возникла ошибка " + ex.Message);
+        t = 0.0;
+        return;
+    }
+    catch (OverflowException ex)
+    {
+        Console.WriteLine("Возникла ошибка " + ex.Message);
+        t = 0.0;
+        return;
+    }
+    sum += t;
+}
+Console.WriteLine($"Среднее арифметическое:{sum / n}");
